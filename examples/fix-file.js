2260395141c256db5beb1f8b8e8edaa0eeba0a6d
jmhmd <jhostetter@gmail.com>
2020-09-07 16:12:57 -0400

    Streamline fixing, don't need to naturalize or namify dataset before writing back

--- examples/fix-file.js
+++ examples/fix-file.js
@@ -2,7 +2,8 @@ const path = require('path');
 const { fix } = require('../');
 
 // const file = path.join(__dirname, '../dcm/seriesuid in sequence/1.dcm');
-const file = path.join(__dirname, '../dcm/corrupt us/1.dcm');
+// const file = path.join(__dirname, '../dcm/corrupt us/1.dcm');
+const file = path.join(__dirname, '../dcm/corrupt us/cine.dcm');
 
 // dump({ path: file, ignoreErrors: false, naturalize: true, stopAfterTag: '7FE00010' })
 fix({ path: file, outputPath: path.join(__dirname, '../dcm/corrupt us/fixed.dcm') })
--- src/fix-dicom.js
+++ src/fix-dicom.js
@@ -10,21 +10,22 @@ const util = require('util');
 const dcmjs = require('@orionmd/dcmjs');
 
 const readFile = util.promisify(fs.readFile);
-const bufferToArrayBuffer = require('./util/buffer-to-arraybuffer');
 
 async function fix(options = {}) {
   const { path: filePath, outputPath } = options;
-  const fileBuffer = await readFile(filePath);
-  const fileArrayBuffer = bufferToArrayBuffer(fileBuffer);
-  const dicomData = dcmjs.data.DicomMessage.readFile(fileArrayBuffer, {
+  // const fileBuffer = fs.readFileSync(filePath).buffer;
+  const file = await readFile(filePath);
+  const dicomData = dcmjs.data.DicomMessage.readFile(file.buffer, {
     ignoreErrors: true,
     stopAfterTag: '7FE00010',
   });
-  const dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
-  // eslint-disable-next-line no-underscore-dangle
-  dataset._meta = dcmjs.data.DicomMetaDictionary.namifyDataset(dicomData.meta);
-  const buffer = dcmjs.data.datasetToBuffer(dataset);
-  fs.writeFileSync(outputPath, buffer);
+  // const dataset = dcmjs.data.DicomMetaDictionary.naturalizeDataset(dicomData.dict);
+  // // eslint-disable-next-line no-underscore-dangle
+  // dataset._meta = dcmjs.data.DicomMetaDictionary.namifyDataset(dicomData.meta);
+  // console.log(dataset.FrameIncrementPointer);
+  // const buffer = dcmjs.data.datasetToBuffer(dataset);
+  // fs.writeFileSync(outputPath, buffer);
+  fs.writeFileSync(outputPath, Buffer.from(dicomData.write()));
 }
 
 module.exports = fix;
